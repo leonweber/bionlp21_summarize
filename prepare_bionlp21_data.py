6f79a653b9a1239b3e7eaf78e7eaf8fef9590dcf
Mario Sänger <saengema@informatik.hu-berlin.de>
2021-01-21 14:29:48 +0100

    Revise data pre-processing script to generate 10 Folds

--- prepare_bionlp21_data.py
+++ prepare_bionlp21_data.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
 
-from pathlib import Path
 
 from numpy import quantile
 from numpy.ma import mean
 from pandas import DataFrame
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from transformers import AutoTokenizer
+from pathlib import Path
 
 
 def clean_text(test: str) -> str:
@@ -18,25 +18,25 @@ def infer_question_type(target: str) -> str:
     return target.split()[0].lower()
 
 
-def norm_question_type(data: DataFrame):
+def norm_question_type(data: DataFrame, min_threshold: int):
     counts = data["question_word"].value_counts()
 
     def _perform_norm_question_word(word: str) -> str:
-        if counts[word] < 2:
+        if counts[word] <= min_threshold:
             return "<rare>"
         return word
 
     return _perform_norm_question_word
 
 
-def read_and_clean_data(input_file: Path) -> DataFrame:
+def read_and_clean_data(input_file: Path, min_threshold: int) -> DataFrame:
     data = pd.read_csv(input_file, sep="\t")
     data = data.rename(columns={"CHQ": "source", "Summary": "target"})
     data["source"] = data["source"].apply(clean_text)
     data["target"] = data["target"].apply(clean_text)
 
     data["question_word"] = data["target"].apply(infer_question_type)
-    data["question_word_norm"] = data["question_word"].apply(norm_question_type(data))
+    data["question_word_norm"] = data["question_word"].apply(norm_question_type(data, min_threshold))
 
     return data
 
@@ -64,6 +64,19 @@ def print_stats(data: DataFrame):
     print(f"Source-WP q95: {quantile(source_lengths, 0.95)}")
     print(f"Source-WP q98: {quantile(source_lengths, 0.98)}")
 
+def save_texts(data: DataFrame, output_dir: Path, filename_prefix: str):
+    source_file = output_dir / f"{filename_prefix}.source"
+    target_file = output_dir / f"{filename_prefix}.target"
+
+    source_writer = source_file.open("w")
+    target_writer = target_file.open("w")
+
+    for id, row in data.iterrows():
+        source_writer.write(row["source"] + "\n")
+        target_writer.write(row["target"] + "\n")
+
+    source_writer.close()
+    target_writer.close()
 
 
 def save_dataset(data: DataFrame, output_dir: Path):
@@ -73,22 +86,27 @@ def save_dataset(data: DataFrame, output_dir: Path):
     splits = {"train": train, "val": val}
 
     for split, split_data in splits.items():
-        source_file = output_dir / f"{split}.source"
-        target_file = output_dir / f"{split}.target"
+        save_texts(split_data, output_dir, split)
 
-        source_writer = source_file.open("w")
-        target_writer = target_file.open("w")
 
-        for id, row in split_data.iterrows():
-            source_writer.write(row["source"] + "\n")
-            target_writer.write(row["target"] + "\n")
 
-        source_writer.close()
-        target_writer.close()
+def generate_and_save_splits(data: DataFrame, seed: int, output_dir: Path):
+    kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
+
+    for i, (train_idx, test_idx) in enumerate(kfold.split(data, data["question_word_norm"])):
+        train_data = data.loc[train_idx]
+        test_data = data.loc[test_idx]
+
+        fold_output_dir = output_dir / f"fold_{i}"
+        fold_output_dir.mkdir(parents=True, exist_ok=True)
+
+        save_texts(train_data, fold_output_dir, "train")
+        save_texts(test_data, fold_output_dir, "test")
 
 
 if __name__ == "__main__":
-    data = read_and_clean_data(Path("data/MeQSum_ACL2019_BenAbacha_Demner-Fushman.csv"))
+    data = read_and_clean_data(Path("data/MeQSum_ACL2019_BenAbacha_Demner-Fushman.csv"), 10)
     print_stats(data)
 
-    save_dataset(data, Path("data/50_50"))
+    #save_dataset(data, Path("data/50_50"))
+    generate_and_save_splits(data, 777, Path("data/splits_s777"))
