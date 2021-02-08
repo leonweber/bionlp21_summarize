6b387631dd23ffa1fd2463dfd4e5f6c8d03d7e87
Mario Sänger <saengema@informatik.hu-berlin.de>
2021-02-08 21:11:20 +0100

    Add options to save model in run_cv.sh

--- run_cv.sh
+++ run_cv.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 TEMP_DIR=tmp
+EPOCHS=25
+COPY_MODEL=true
 
 DATA_DIR=$1
 OUTPUT_DIR=$2
 
 for i in {0..9}
 do
-  rm -rf TEMP_DIR
+  rm -rf TEMP_DIR/*
 
   FOLD_DIR=$DATA_DIR/fold_$i
   SOURCE_FILE=$FOLD_DIR/test.source
@@ -25,7 +27,7 @@ do
     --evaluation_strategy epoch \
     --predict_with_generate \
     --overwrite_output_dir \
-    --num_train_epochs 25
+    --num_train_epochs $EPOCHS
 
   # Run prediction on fold i
   echo "Running prediction for fold $i"
@@ -37,6 +39,16 @@ do
   # Run evaluation on fold i
   echo "Running evaluation for fold $i"
   python rouge_cli.py $PRED_FILE $TARGET_FILE > $OUTPUT_DIR/result_$i.txt
+
+  if [ "$COPY_MODEL" = true ] ; then
+    MODEL_DIR=$OUTPUT_DIR/model_$i/
+    mkdir -p $MODEL_DIR
+
+    for file in config.json pytorch_model.bin vocab.json tokenizer_config.json special_tokens_map.json merges.txt
+    do
+      cp $TEMP_DIR/$file $MODEL_DIR/$file
+    done
+  fi
 done
 
 # Aggregate all fold results
