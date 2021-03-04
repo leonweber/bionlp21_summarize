958f4ffd6a465b61debe75ab76edc0e3cb9de8bd
Leon Weber <leonweber@posteo.de>
2021-03-04 12:46:16 +0100

    Fix bug

--- train_rouge_predictor.py
+++ train_rouge_predictor.py
@@ -8,11 +8,12 @@ from typing import List
 from pytorch_lightning import seed_everything
 from sentence_transformers import CrossEncoder, InputExample
 from sentence_transformers.evaluation import SentenceEvaluator, SequentialEvaluator
-from torch.nn import ReLU
+import torch
+from torch import nn
+from torch.nn import functional as F
 from torch.utils.data import DataLoader
 
 from run_eval_sent_transformer import read_examples
-from train_sent_transformer import RougeEvaluator
 from utils import calculate_rouge
 
 
@@ -46,12 +47,13 @@ class RougeEvaluator(SentenceEvaluator):
 
     def __call__(self, model: CrossEncoder, output_path: str = None, epoch: int = -1, steps: int = -1) -> float:
         # Run rouge score prediction
+        model.model.eval()
         scores = model.predict([example.texts for example in self.val_examples],
                                batch_size=self.batch_size, show_progress_bar=True)
 
         # Get example with highest score per id
         best_instances = {}
-        best_scores = defaultdict(lambda x: -1)
+        best_scores = defaultdict(lambda: -10000)
 
         for example, score in zip(self.val_examples, scores):
             if score > best_scores[example.guid]:
@@ -92,42 +94,54 @@ class RougeEvaluator(SentenceEvaluator):
 def train_sentence_transformer(
         model_name: str,
         input_file: Path,
-        gold_targets_file: Path,
+        val_data: Path,
+        candidate_file_val: Path,
         output_dir: Path,
         lower_case: bool,
         epochs: int,
-        batch_size: int
+        batch_size: int,
+        eval_steps: int,
+        lr: float
 ):
     model = CrossEncoder(model_name, num_labels=1, max_length=512)
 
+    acitvation_fct = torch.tanh
+
     train_examples = read_examples(input_file, lower_case)
+    for example in train_examples:
+        if acitvation_fct == torch.tanh:
+            example.label = example.label / 50 - 1 # normalize to [-1, 1]
+        elif acitvation_fct == torch.sigmoid:
+            example.label = example.label / 100 # normalize to [0, 1]
+
+    source_file_val = Path(str(val_data) + ".source")
+    target_file_val = Path(str(val_data) + ".target")
+
     train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=batch_size)
 
-    if lower_case:
-        gold_targets = [line.strip().lower() for line in gold_targets_file.open("r").readlines()]
-    else:
-        gold_targets = [line.strip() for line in gold_targets_file.open("r").readlines()]
-
     seq_evaluator = SequentialEvaluator(evaluators=[
-        RougeEvaluator(train_examples, gold_targets)
+        RougeEvaluator(source_file=source_file_val,
+                       target_file=target_file_val,
+                       candidate_file=candidate_file_val
+                       )
     ])
 
     optimizer_params = {
-        "lr": 2e-2,
-        "eps": 1e-6,
+        "lr": lr,
         "correct_bias": False
     }
 
     model.fit(
         train_dataloader,
         evaluator=seq_evaluator,
-        acitvation_fct=ReLU(),
+        acitvation_fct=acitvation_fct,
         epochs=epochs,
         warmup_steps=100,
+        evaluation_steps=eval_steps,
         output_path=str(output_dir),
         optimizer_params=optimizer_params,
         save_best_model=True,
-
+        loss_fct=nn.MSELoss()
     )
 
 
@@ -135,13 +149,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, required=True)
     parser.add_argument("--input_file", type=Path, required=True)
-    parser.add_argument("--target_file", type=Path, required=True)
+    parser.add_argument("--val_data", type=Path, required=True)
+    parser.add_argument("--candidate_file_val", type=Path, required=True)
     parser.add_argument("--output_dir", type=Path, required=True)
 
     parser.add_argument("--epochs", type=int, default=10, required=False)
-    parser.add_argument("--batch_size", type=int, default=32, required=False)
+    parser.add_argument("--eval_steps", type=int, default=0, required=False)
+    parser.add_argument("--batch_size", type=int, default=16, required=False)
     parser.add_argument("--cased", type=bool, default=False, required=False)
     parser.add_argument("--seed", type=int, default=42, required=False)
+
+    parser.add_argument("--lr", type=float, required=True)
 
     args = parser.parse_args()
 
@@ -150,9 +168,12 @@ if __name__ == "__main__":
     train_sentence_transformer(
         model_name=args.model,
         input_file=args.input_file,
-        gold_targets_file=args.target_file,
+        val_data=args.val_data,
+        candidate_file_val=args.candidate_file_val,
         output_dir=args.output_dir,
         lower_case=not args.cased,
         epochs=args.epochs,
-        batch_size=args.batch_size
+        batch_size=args.batch_size,
+        eval_steps=args.eval_steps,
+        lr=args.lr
     )
